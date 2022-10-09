96aa4f71f3f2423d9a04e1896da7aadb9dbe73d3
shajlee <chowdhurygs@gmail.com>
2022-10-08 22:01:02 -0700

    first commit

---
+++ src/practice_basic_assignment/My_First_Assignment.java
@@ -0,0 +1,51 @@
+package practice_basic_assignment;
+
+public class My_First_Assignment {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("It Is Raining Today");
+		System.out.println("The Rain Is Beautiful");
+		System.out.println("The Dark Clouds");
+		System.out.println("The Wet Fields");
+		System.out.println("The Rumbling of the Thunder");
+		System.out.println("Truly Takes my Breath Away");
+		System.out.println("There is Nothing More Majestic");
+		System.out.println("Than a Heavy Rainfall");
+		System.out.println("It Feels Like the Sky is Calling Us");
+		System.out.println("To Celebrate Life");
+		System.out.println("And Cherish What We Have");
+		System.out.println("The Rain Cleanses The Earth");
+		System.out.println("The Rain Washes Away Our Worries");
+		System.out.println("The Rain Gives Us a Respite");
+		System.out.println("Its Amazing to Smell the Rain");
+		System.out.println("Its Amazing to Feel the Wind");
+		System.out.println("Its Amazing to Touch the Drops");
+		System.out.println("Because in Every Life");
+		System.out.println("Some Rain Must Fall");
+		System.out.print("Arya Stark's");
+		System.out.print("Death List:");
+		System.out.print("Walder Frey,");
+		System.out.print("Cersei Lannister,");
+		System.out.print("Tywin Lannister,");
+		System.out.print("Joffrey Baratheon,");
+		System.out.print("Meryn Trant,");
+		System.out.print("Sandor 'The Hound' Clegane,");
+		System.out.print("Gregor 'The Mountain' Clegane,");
+		System.out.print("Polliver,");
+		System.out.print("Rorge,");
+		System.out.print("Melisandre,");
+		System.out.print("Beric Dondarrion,");
+		System.out.print("Thoros of Myr,");
+		System.out.print("Ilyn Payne.");
+		System.out.print("Finally,");
+		System.out.print("a girl is no one.");
+		System.out.print("The girl is Arya Stark of Winterfell,");
+		System.out.print("and she is");
+		System.out.print(" going home.");
+		
+		
+
+	}
+
+}
---
+++ src/practice_basic_assignment/My_First_Assignment_1.java
@@ -0,0 +1,123 @@
+package practice_basic_assignment;
+
+public class My_First_Assignment_1 {
+
+	public static void main(String[] args) {
+	    int a = 1;
+	    int b = 2;
+	    int c = 3;
+	    int d = 40;
+	    int e = 55;
+	    int f = 666;
+	    int g = 7891;
+	    int h = 87654;
+	    int i = -96;
+	    int j = -85;
+	    System.out.println(a);
+	    System.out.println(b);
+	    System.out.println(c);
+	    System.out.println(d);
+	    System.out.println(e);
+	    System.out.println(f);
+	    System.out.println(g);
+	    System.out.println(h);
+	    System.out.println(i);
+	    System.out.println(j);
+	    
+	    boolean b1=1<2;
+	    System.out.println(b1);
+	    boolean b2=191.123<2;
+	    System.out.println(b2);
+	    boolean b3=true;
+	    boolean b4=false;
+	    boolean b5=(b3==b4);
+	    System.out.println(b5);
+	    boolean b6=5>4;
+	    System.out.println(b6);
+	    boolean b7=174.456>172;
+	    System.out.println(b7);
+	    boolean b8=false;
+	    boolean b9=false;
+	    boolean b10=(b8==b9);
+	    System.out.println(b10);
+	    boolean b11=6==6;
+	    System.out.println(b11);
+	    boolean b12=150.01==150.01;
+	    System.out.println(b12);
+	    boolean b13=100<110;
+	    System.out.println(b13);
+	    boolean b14=486.99>413.99;
+	    System.out.println(b14);
+	    
+	    char character;
+	    
+	    character = 'r';
+	    System.out.println(character);
+	    character = '@';
+	    System.out.println(character);
+	    character = '2';
+	    System.out.println(character);
+	    character = 'q';
+	    System.out.println(character);
+	    character = '#';
+	    System.out.println(character);
+	    character = '8';
+	    System.out.println(character);
+	    character = 'h';
+	    System.out.println(character);
+	    character = '$';
+	    System.out.println(character);
+	    character = '5';
+	    System.out.println(character);
+	    character = 'x';
+	    System.out.println(character);
+	    
+	    double d1=10.6;
+	    System.out.println(d1);
+	    double d2=5.8;
+	    System.out.println(d2);
+	    double d3=40.96;
+	    System.out.println(d3);
+	    double d4=50.897;
+	    System.out.println(d4);
+	    double d5=60.8971;
+	    System.out.println(d5);
+	    double d6=70.89736;
+	    System.out.println(d6);
+	    double d7=80.897369;
+	    System.out.println(d7);
+	    double d8=90.145694;
+	    System.out.println(d8);
+	    double d9=100.2569741;
+	    System.out.println(d9);
+	    double d10=200.32165498;
+	    System.out.println(d10);
+	    
+	    
+	  
+	   
+	    
+	    
+	    
+	    
+
+	    
+	    
+	    
+	   
+	   
+	    
+	    
+	    
+	    
+	   
+	   
+	    
+	    		
+	    
+	    		
+	    
+
+	}
+
+}
